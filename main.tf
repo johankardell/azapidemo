3cb8c69078be298e7e7fbfc1d8c2902d50635355
Johan Kardell <johan.kardell@gmail.com>
2022-04-13 15:28:47 +0200

    Add storage account sample

--- main.tf
+++ main.tf
@@ -20,3 +20,50 @@ resource "azapi_resource" "automationAccount" {
     }
   })
 }
+
+
+resource "azapi_resource" "storage" {
+  type      = "Microsoft.Storage/storageAccounts@2021-08-01"
+  name      = "saazapidemo"
+  parent_id = azurerm_resource_group.azapi.id
+
+  location = azurerm_resource_group.azapi.location
+  body = jsonencode({
+    properties = {
+      accessTier : "Hot",
+      minimumTlsVersion : "TLS1_2"
+      supportsHttpsTrafficOnly : true
+      allowBlobPublicAccess : false
+      allowSharedKeyAccess : false
+      allowCrossTenantReplication : false
+      defaultToOAuthAuthentication : false
+      networkAcls : {
+        bypass : "AzureServices"
+        defaultAction : "Deny"
+        ipRules : []
+      },
+      encryption : {
+        keySource : "Microsoft.Storage"
+        services : {
+          blob : {
+            enabled : true
+          },
+          file : {
+            enabled : true
+          },
+          table : {
+            enabled : true
+          },
+          queue : {
+            enabled : true
+          }
+        },
+        requireInfrastructureEncryption : true
+      }
+    },
+    sku : {
+      name : "Standard_LRS"
+    },
+    kind : "StorageV2"
+  })
+}
